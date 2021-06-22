c7dc8a30edadd6f191f64f4961f7c39b99703ea6
MyUsername <emanuele.marini37@gmail.com>
2021-06-22 17:43:27 +0200

    css html

--- js/script.js
+++ js/script.js
@@ -107,3 +107,37 @@ const icons = [
 		family: 'fas'
 	}
 ];
+
+/*
+Milestone 1
+Partendo dalla seguente struttura dati , mostriamo in pagina tutte le icone disponibili come da layout.
+
+
+in brutta vediamo le icone sfrtuttando le proprietà che rimandano alla costruzione di fontawesome
+*/
+
+const containerIcoHTML = document.querySelector('.icons-content');
+
+icons.forEach((icon) => {
+
+    const { name, prefix, family } = icon;
+
+      containerIcoHTML.innerHTML += `
+
+            <div class="icon-card col>
+            <i class="${family} ${prefix}${name}"</i>
+            </div>
+            
+            
+            `    
+      
+      
+
+
+
+
+
+
+});
+
+
